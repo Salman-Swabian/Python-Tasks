8eb8df7a6c45a264950fc8cac4b85bd919cc53c8
Salman-Swabian <126056840+Salman-Swabian@users.noreply.github.com>
2023-02-25 13:21:00 +0500

    Add files via upload

---
+++ lecture2.py
@@ -0,0 +1,17 @@
+# Task 1: write a program to print 5 intergers numbers,5 floating numbers,5 complex numbers
+i=[1,2,3,4,5]#5 integers numbers
+f=[1.2,2.34,4.66,3.1,7.4 ]#5 Floating numbers
+c=[5j,6j,2j,4j,8j]#5 complex numbers
+print(i)
+print(f)
+print(c)
+#Task 2 check the data type of task 1
+print(type(i))
+print(type(f))
+print(type(c))
+#Task 3:Write a program to store your name and store the names of your family members and check its data type
+a={'Salman Shah','ahmad','hasnain','saim'} 
+print("Data Type of family members is :", type(a))
+#Task 4: write a program to store introductory pragraph about yourself
+intro={'My name is Salman Shah ,I belong to Swabi,I have done matric from "The Readers school"  and obtained A+ Grade,Now I am a computer science student in "University Of Engineering and Technology Peshawar"'}
+print(intro)
